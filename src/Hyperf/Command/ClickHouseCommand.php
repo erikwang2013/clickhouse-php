3b4d05c178573c7e1d9af235246cf8d9e87bfdfb
erik <erik@erik.xyz>
2026-05-27 17:22:37 +0800

    feat: add Hyperf adapter with SwoolePool, DI, and ConfigProvider

---
+++ src/Hyperf/Command/ClickHouseCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Erikwang2013\ClickHouse\Hyperf\Command;
+
+use Erikwang2013\ClickHouse\Hyperf\ClickHouseConnection;
+use Hyperf\Command\Command;
+
+class ClickHouseCommand extends Command
+{
+    protected ?string $name = 'clickhouse:table-list';
+    protected string $description = 'List all tables in ClickHouse';
+
+    public function __construct(
+        private readonly ClickHouseConnection $clickhouse,
+    ) {
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        $tables = $this->clickhouse->connection()->select('SHOW TABLES');
+        $this->info('ClickHouse Tables:');
+        foreach ($tables as $table) {
+            $this->line('  ' . ($table['name'] ?? reset($table)));
+        }
+    }
+}
---
+++ src/Hyperf/Pool/ClickHousePool.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Erikwang2013\ClickHouse\Hyperf\Pool;
+
+use Erikwang2013\ClickHouse\Pool\SwoolePool;
+
+class ClickHousePool extends SwoolePool
+{
+}
