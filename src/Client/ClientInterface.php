615e0a679bb74c0a4eea93110a89a266ca68d4d4
erik <erik@erik.xyz>
2026-05-27 16:56:37 +0800

    feat: add ClientInterface, HttpClient, and Result class

---
+++ src/Client/ClientInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Erikwang2013\ClickHouse\Client;
+
+use Erikwang2013\ClickHouse\Query\Result;
+
+interface ClientInterface
+{
+    public function query(string $sql, array $bindings = []): Result;
+    public function select(string $sql, array $bindings = []): array;
+    public function insert(string $table, array $data): int;
+    public function ping(): bool;
+}
